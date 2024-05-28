f01eba09c27834c344240938fe3a55e4137eb269
Negar Baharmand <negarbaharmand@outlook.com>
2024-05-28 09:36:21 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/g49jpalecture/G49JpaLectureApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.g49jpalecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G49JpaLectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G49JpaLectureApplication.class, args);
+    }
+
+}
